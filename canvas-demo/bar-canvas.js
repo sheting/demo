79f1dde87f85afa9d03f8ef296b0ebe3e85d643b
xuting <xuting@chainnova.com>
2019-06-18 19:03:01 +0800

    chart point

--- canvas-demo/bar-canvas.js
+++ canvas-demo/bar-canvas.js
@@ -1,27 +1,53 @@
-function barChart(canvas) {
+function barChart(canvas, data, options) {
     this.canvas = document.getElementById(canvas);
     this.ctx = this.canvas.getContext('2d');
     this.width = this.canvas.width;
     this.height = this.canvas.height;
     this.padding = 50;
-    console.log('in-----',  this.canvas, this.ctx, this.width, this.height)
-    this.init()
+    this.bgColor = '#fff';
+    this.axisColor = '#666';
+    this.xLength = 0;// x轴上坐标点之间的长度
+    this.yLength = 0;// y轴上坐标点之间的长度
+    this.data = data;
+    this.dataLength = this.data.length;
+    this.init(options)
 }
 barChart.prototype = {
-    init: function() {
-        this.drawAxis()
+    init: function(options) {
+        if (options) {
+            this.padding = options.padding || 50;
+            this.bgColor = options.bgColor || '#fff';
+            this.axisColor = options.axisColor || '#666';
+        }
+        this.xLength = Math.floor((this.width - this.padding * 2 - 10) / this.dataLength);
+        this.drawAxis();
+        this.drawPoint();
     },
     drawAxis: function() {
         this.ctx.beginPath();
         this.ctx.lineWidth = 1;
-        this.ctx.strokeStyle = '#000000';
+        this.ctx.strokeStyle = this.axisColor;
         // y轴线
         this.ctx.moveTo(this.padding + 0.5, this.height - this.padding + 0.5);
         this.ctx.lineTo(this.padding + 0.5, this.padding + 0.5);
-        this.ctx.stroke();
         // x轴线
         this.ctx.moveTo(this.padding + 0.5, this.height - this.padding + 0.5);
         this.ctx.lineTo(this.width - this.padding + 0.5, this.height - this.padding + 0.5);
+        // 绘制线
+        this.ctx.stroke();
+    },
+    drawPoint: function() {
+        // x轴坐标点
+        this.ctx.beginPath()
+        this.ctx.fillStyle = this.axisColor;
+        this.ctx.textAlign = 'center';
+        for (var i = 0; i < this.dataLength; i++) {
+            var xAxis = this.data[i].xAxis;
+            var xlen = this.xLength * (i + 1);
+            this.ctx.moveTo(this.padding + xlen + 0.5, this.height - this.padding + 0.5);
+            this.ctx.lineTo(this.padding + xlen + 0.5, this.height - this.padding + 5.5);
+            this.ctx.fillText(xAxis, this.padding + xlen - this.xLength / 2, this.height - this.padding + 15);
+        }
         this.ctx.stroke();
     }
 }
